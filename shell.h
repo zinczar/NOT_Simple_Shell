655853416cc0e5fe00f4ee9394698407dceeebd2
zinczar <brendan@wineisneat.com>
2019-08-19 22:44:27 +0000

    updated shell.h

--- shell.h
+++ shell.h
@@ -10,5 +10,9 @@
 int _putchar(char c);
 int main(void);
 char *pathfinder(char *input);
+int _strlen(const char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 
 #endif
---
+++ shell_helpers.c
@@ -0,0 +1,95 @@
+#include "shell.h"
+
+/**
+ * _strlen - code
+ * @s: Char
+ *
+ * Return: Count
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (s[count])
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _strcpy - Code
+ * @dest: Char
+ * @src: Char
+ *
+ * Return: Dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	unsigned int src_len, i;
+
+	src_len = _strlen(src);
+
+	for (i = 0; i < src_len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strcat - check the code for Holberton School students.
+ * @dest: Char
+ * @src: Char
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int strlen = 0;
+	int i;
+
+	while (*dest)
+	{
+		strlen++;
+		dest++;
+	}
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[(strlen - 1) + i] = src[i];
+	}
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: compared strings
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
